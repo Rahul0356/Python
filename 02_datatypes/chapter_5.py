3a33ad17b2f4e4cd6026727c23253dedc25d91e8
Rahul Kumar <rahulkumar9199270356@gmail.com>
2025-11-25 18:46:14 +0530

    chpater-5

---
+++ 02_datatypes/chapter_5.py
@@ -0,0 +1,9 @@
+import sys
+
+ideal_temp = 95.5
+current_temp = 100.0
+
+print(f"Ideal tea brewing temperature is {ideal_temp}")
+print(f"Current tea brewing temperature is {current_temp}")
+print(f"Difference temp {ideal_temp - current_temp}")
+print(sys.float_info)
