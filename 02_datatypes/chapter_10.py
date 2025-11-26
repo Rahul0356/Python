0b6d446d5a4e45298a8915c9f31a71b2f715993c
Rahul Kumar <rahulkumar9199270356@gmail.com>
2025-11-26 18:48:32 +0530

    datatypes

---
+++ 02_datatypes/chapter_10.py
@@ -0,0 +1,14 @@
+chai_order = dict(type="Masala chai", size="Large",suger=2)
+
+print(f"Chai order:{chai_order}")
+
+chai_recipe = {}
+chai_recipe["base"] = "black tea"
+chai_recipe["liquid"] = "milk"
+
+print(f"Recipe base:{chai_recipe[chai_recipe]} ")
+
+chai_order = {"type":"Ginger Chai","size":"Medium","sugar":1}
+
+print(f"Order details (keys):{chai_order.keys()}")
+print(f"Order deatails (values):{chai}")
