1ae5fbb625d8789b00177674af42232d4b6212b9
Rahul Kumar <rahulkumar9199270356@gmail.com>
2025-11-27 09:48:09 +0530

    chai_price_calculations

---
+++ 03_conditionals/chai_price_cal.py
@@ -0,0 +1,11 @@
+cup = input ("Enter the price of the size(small/meduim/large)")
+
+if cup == "small":
+    print("the price of small chai is 10rs")
+elif cup == "meduim":
+    print("the price of medium chai is 15rs")
+elif cup == "large":
+    print("the price of large chai is 20 rs")
+else:
+    print("invalid size")
+    
