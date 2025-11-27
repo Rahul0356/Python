1204755fb04caf0af17a88bb6b9f215e5554a564
Rahul Kumar <rahulkumar9199270356@gmail.com>
2025-11-27 09:38:37 +0530

    snack_suggestion

---
+++ 03_conditionals/snack_suggestion_2.py
@@ -0,0 +1,7 @@
+snack = input("Enter your prefreed snak:")
+
+if snack == "cookies" or snack == "somasa":
+    print(f"Great Choice! We'll serve you {snack}")
+
+else:
+    print("We only serve cookies and somasa");
